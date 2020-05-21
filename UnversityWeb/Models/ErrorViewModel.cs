366d3f75cadb8269f63dd552df754ff9a968c497
taky1 <48655485+taky1@users.noreply.github.com>
2020-05-21 01:45:47 +0100

    Ajoutez des fichiers projet.

---
+++ UnversityWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UnversityWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
